ab27fad259bba8a7b52914fd6583c964b4a78970
David Parker <david@goparker.com>
2013-01-26 16:14:29 +0000

    Added code for making fire sound for bullets

--- HeartAttack/HeartAttack/BulletManager.cs
+++ HeartAttack/HeartAttack/BulletManager.cs
@@ -3,16 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
 
 namespace HeartAttack
 {
     public class BulletManager
     {
+        public SoundEffect m_FireSound;
         private List<Bullet> m_Bullets;
 
         public BulletManager()
         {
             m_Bullets = new List<Bullet>();
+            m_FireSound = HeartAttack.theGameInstance.Content.Load<SoundEffect>("fire");
         }
 
         public List<Bullet> Bullets
@@ -25,6 +28,7 @@ namespace HeartAttack
             if (pBullet != null)
             {
                 m_Bullets.Add(pBullet);
+                m_FireSound.Play();
             }
         }
 
